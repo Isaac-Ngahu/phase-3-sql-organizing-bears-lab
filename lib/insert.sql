2e27eace79c8ce5bf16bd1c94ebab3002b6cab92
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-07-07 15:43:36 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Mr. Chocolate",3,"M","brown","cool",true);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Rowdy",4,"F","white","cold",true);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Tabitha",5,"M","yellow","dry",false);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Sergeant Brown",6,"F","black","dry",false);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Melissa",7,"M","black","cold",true);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Grinch",8,"F","white","cold",false);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Wendy",9,"M","dark brown","dry",true);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES(NULL,3,"F","grey","cool",true);
